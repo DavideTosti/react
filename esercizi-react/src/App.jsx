64fa16a98d04323ac6787e6e6d3152b724c5c982
DavideTosti <davidetosti2002@gmail.com>
2025-04-03 09:33:25 +0200

    corretto funzionamento esercizio

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
 import Welcome from "./Welcome";
 import { Counter } from "./Counter";
 import GithubUserList from "./GithubUserList";
@@ -6,21 +6,23 @@ import ShowGitHubUser from "./ShowGithubUser";
 
 export default function App() {
   return (
-    <div>
+    <BrowserRouter>
       <div>
-        <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> |{" "}
-        <Link to="/users">Users</Link>
-      </div>
+        <div>
+          <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> |{" "}
+          <Link to="/users">Users</Link>
+        </div>
 
-      <Routes>
-        <Route path="/" element={<Welcome name="Guest" />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/users" element={<GithubUserList />}>
-          <Route index element={<p>Add user</p>} />
-          <Route path=":username" element={<ShowGitHubUser />} />
-        </Route>
-        <Route path="*" element={<p>User not found</p>} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Welcome name="Guest" />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/users" element={<GithubUserList />}>
+            <Route index element={<p>Add user</p>} />
+            <Route path=":username" element={<ShowGitHubUser />} />
+          </Route>
+          <Route path="*" element={<p>User not found</p>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
